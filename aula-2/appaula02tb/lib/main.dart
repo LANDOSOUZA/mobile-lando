58f56c8cdb23a164362719b6cdeae479db94b6a7
LANDOSOUZA <lando-souza@hotmail.com>
2026-02-12 22:10:41 -0300

    Update main.dart

--- aula-2/appaula02tb/lib/main.dart
+++ aula-2/appaula02tb/lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
@@ -28,9 +29,9 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellow),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'App aula 02 - Mobile'),
     );
   }
 }
@@ -102,10 +103,17 @@ class _MyHomePageState extends State<MyHomePage> {
           // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
           // action in the IDE, or press "p" in the console), to see the
           // wireframe for each widget.
+          // mainAxisAlignment - posicionamento no centro
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Text("SENAI",style: TextStyle(fontSize:20,fontWeight: FontWeight.w800),),
             const Text('You have pushed the button this many times:'),
             Text(
+            Container(
+              width: 50,
+              height: 50,
+              color: colors.blue,
+              child: Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
